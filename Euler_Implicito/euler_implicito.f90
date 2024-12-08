de06d43a455113c755125368584fa50a059125c2
efesic <miguel.apestegui03@gmail.com>
2024-12-08 16:27:22 +0100

    Resolt Euler implícit

--- Euler_Implicito/euler_implicito.f90
+++ Euler_Implicito/euler_implicito.f90
@@ -1,74 +1,175 @@
-program euler_implicito
-
-! Consideramos un disco uniforme de altura h como el tejido enfermo
-! c_v es calor específico, p la densidad, k la conductividad térmica
-! i dif la difusión térmica
-INTEGER, PARAMETER :: c_v=3686, p=1081, h=0.5, ampven=2
-REAL, PARAMETER :: k=0.56, phi=0.472, dif=k/c_v/p
-
-!Las condiciones iniciales i de frontera. El sistema se encuentra a la temperatura
-!del cuerpo humano Tc inicialmente y en Tc siempre en la frontera.
-!N es el nombre de puntos de espacio, ax la amplitud del espaciado en el espacio
-!i at el espaciado en el tiempo.
+PROGRAM euler_implicito
+INTEGER, PARAMETER :: c_v=3686, p=1081, ampven=2
+REAL, PARAMETER :: k=0.56, phi=0.472, dif=k/c_v/p, h=0.5
 INTEGER, PARAMETER :: N=101
-CHARACTER(len=N) :: formato
 REAL, PARAMETER :: Tc=36.5, tin=0, tfin=0.025,xin=0,xfin=ampven
 REAL, PARAMETER :: ax=(xfin-xin)/N, at1=(ax)**2, at2=0.5*(ax)**2
-REAL, PARAMETER :: gamma_1=1.0, gamma_2=0.5
-REAL, PARAMETER :: beta_1=1/3, beta_2=0.5
-INTEGER :: Nat1 = int((tfin-tin)/at1), Nat2=int((tfin-tin)/at2)
-REAL, ALLOCATABLE :: T(:,:), xlin(:), tlin(:)
+INTEGER :: Nat1 = int((tfin-tin)/at1), Nat2 = int((tfin-tin)/at2), info
+INTEGER, ALLOCATABLE :: ipiv(:)
+REAL, ALLOCATABLE :: T(:,:), xlin(:), tlin(:), M(:,:), M_inv(:,:), b(:), T_new(:), work(:)
 
+!----///---PRIMER CAS---///---:
+!Es crea la malla de punts d'espai i temps
+ALLOCATE(xlin(N), tlin(Nat1))
 xlin = linspace(xin,xfin,N)
 tlin = linspace(tin,tfin,Nat1)
 
-!Estructura T(i,j): i és la component espaial, j la component temporal
-ALLOCATE(T(N,Nat1))
+!Calculem la matriu M i la seva inversa:
+ALLOCATE(M(N-2,N-2),M_inv(N-2,N-2),ipiv(N-2),work(2*(N-2)))
+M = 0.0
+DO i = 1, N-2
+    IF (i > 1) M(i, i-1) = -1.0     
+    M(i, i) = 3.0                 
+    IF (i < N-2) M(i, i+1) = -1.0   
+END DO
+CALL invertir(M, M_inv, N-2, ipiv, work, info)
+
+!Condició que informa si la matriu construida és invertible o no
+IF (info /= 0) THEN
+    PRINT *, "La matriu no és invertible, info =", info
+END IF
+!Vector constant que conté les condicions inicials per a les iteracions
+ALLOCATE(b(N-2))
+b = at1
+b(1)= Tc + at1
+b(N-2)= Tc + at1
+
+!Estructura del mapa de temperatures T(i,j): i és la component temporal, j la component espaial
+ALLOCATE(T(Nat1,N))
+
 !Condicions de contorn
-T(1,:)=Tc
-T(N,:)=Tc
+T(:,1)=Tc
+T(:,N)=Tc
 
 !Condicions inicials
-T(:,1)=Tc
-!
-DO i=2,N-1
-    DO j=2,Nat1-1
-        T(i,j)= beta_1*T(1,j-1) + beta_1*gamma_1*T(i-1,j) + beta_1*gamma_1*T(i+1,j) + beta_1*at1
-    END DO
-END DO
+T(1,:)=Tc
+ALLOCATE(T_new(N-2))
 
+!Iteracions aplicant l'equació matricial
+DO i=2,Nat1
+    T_new = matmul(M_inv,T(i-1,2:N-1)+b)
+    T(i,2:N-1) = T_new
+END DO
+DEALLOCATE(T_new)
+
+!Imprimim les dades com a llistes de dades que pugui entendre gnuplot o altre programa que les grafiqui:
 PRINT*, "> Open dades_3D_imp_at1"
 OPEN(unit=10,file='dades_3D_imp_at1.txt',status='replace',action='write')
 PRINT*,"> Open dades_2D_imp_at1"
 OPEN(unit=20,file='dades_2D_imp_at1.txt',status='replace',action='write')
+
 !Dades gràfic 3D:
 DO i=1,N
     DO j=1,Nat1
-        WRITE(10, '(F10.6,F10.6,F10.3)') xlin(i), tlin(j), T(i,j)
+        WRITE(10, '(F10.6,F10.6,F10.3)') xlin(i), tlin(j), T(j,i)
     END DO
 END DO
+
 !Dades gràfic 2D a y =0.025
-DO i=1,Nat1
-    WRITE(20,'(F10.6,F10.6)') xlin(i), T(i,Nat1)
+DO i=1,N
+    WRITE(20,'(F10.6,F10.6)') xlin(i), T(Nat1,i)
 END DO
 CLOSE(10)
 PRINT *, "> 'dades_3D_imp_at1' closed correctly"
 CLOSE(20)
 PRINT *, "> 'dades_2D_imp_at1' closed correctly"
+DEALLOCATE(xlin,tlin,M,M_inv,ipiv,work,b,T)
 
-contains
-    function linspace(valin, valfin, num) result(lista)
-        real, intent(in) :: valin, valfin   ! Valor final i inicial
-        integer, intent(in) :: num ! Número de puntos
-        real, allocatable :: lista(:)
-        integer :: k
-        real :: paso
+!---///---SEGON CAS---///---:
+!Es crea la malla de punts d'espai i temps
+ALLOCATE(xlin(N), tlin(Nat2))
+xlin = linspace(xin,xfin,N)
+tlin = linspace(tin,tfin,Nat2)
+
+!Calculem la matriu M i la seva inversa:
+ALLOCATE(M(N-2,N-2),M_inv(N-2,N-2),ipiv(N-2),work(2*(N-2)))
+M = 0.0
+DO i = 1, N-2
+    IF (i > 1) M(i, i-1) = -0.5     
+    M(i, i) = 2.0                 
+    IF (i < N-2) M(i, i+1) = -0.5   
+END DO
+CALL invertir(M, M_inv, N-2, ipiv, work, info)
+
+!Condició que informa si la matriu construida és invertible o no
+IF (info /= 0) THEN
+    PRINT *, "La matriu no és invertible, info =", info
+END IF
+!Vector constant que conté les condicions inicials per a les iteracions
+ALLOCATE(b(N-2))
+b = at2
+b(1)= 0.5*Tc + at2
+b(N-2)= 0.5*Tc + at2
+
+!Estructura del mapa de temperatures T(i,j): i és la component temporal, j la component espaial
+ALLOCATE(T(Nat2,N))
+
+!Condicions de contorn
+T(:,1)=Tc
+T(:,N)=Tc
+
+!Condicions inicials
+T(1,:)=Tc
+ALLOCATE(T_new(N-2))
+
+!Iteracions aplicant l'equació matricial
+DO i=2,Nat2
+    T_new = matmul(M_inv,T(i-1,2:N-1)+b)
+    T(i,2:N-1) = T_new
+END DO
+DEALLOCATE(T_new)
+
+!Imprimim les dades com a llistes de dades que pugui entendre gnuplot o altre programa que les grafiqui:
+PRINT*, "> Open dades_3D_imp_at2"
+OPEN(unit=10,file='dades_3D_imp_at2.txt',status='replace',action='write')
+PRINT*,"> Open dades_2D_imp_at2"
+OPEN(unit=20,file='dades_2D_imp_at2.txt',status='replace',action='write')
+
+!Dades gràfic 3D:
+DO i=1,N
+    DO j=1,Nat2
+        WRITE(10, '(F10.6,F10.6,F10.3)') xlin(i), tlin(j), T(j,i)
+    END DO
+END DO
+
+!Dades gràfic 2D a y =0.025
+DO i=1,N
+    WRITE(20,'(F10.6,F10.6)') xlin(i), T(Nat2,i)
+END DO
+CLOSE(10)
+PRINT *, "> 'dades_3D_imp_at2' closed correctly"
+CLOSE(20)
+PRINT *, "> 'dades_2D_imp_at2' closed correctly"
+DEALLOCATE(xlin,tlin,M,M_inv,ipiv,work,b,T)
+
+!Subrutines que es cridarem al executar el programa
+CONTAINS
+    FUNCTION linspace(valin, valfin, num) result(lista)
+        REAL, INTENT(in) :: valin, valfin   
+        INTEGER, INTENT(in) :: num 
+        REAL, ALLOCATABLE :: lista(:)
+        INTEGER :: k
+        REAL :: paso
         
         paso = (valfin - valin) / (num - 1)
-        allocate(lista(num))
+        ALLOCATE(lista(num))
         
-        do k = 1, num
+        DO k = 1, num
             lista(k) = valin + paso*(k - 1)
-        end do
-    end function linspace
-END program euler_implicito
+        END DO
+    END FUNCTION linspace
+    SUBROUTINE invertir(A, A_inv, n, ipiv, work, info)
+        REAL, INTENT(in) :: A(:,:)          
+        REAL, INTENT(out) :: A_inv(:,:)     
+        INTEGER, INTENT(in) :: n           
+        INTEGER, INTENT(out) :: ipiv(:)     
+        REAL, INTENT(out) :: work(:)       
+        INTEGER, INTENT(out) :: info       
+
+        A_inv = A
+        CALL sgetrf(n, n, A_inv, n, ipiv, info)
+        IF (info /= 0) RETURN 
+
+        CALL sgetri(n, A_inv, n, ipiv, work, SIZE(work), info)
+    END SUBROUTINE invertir
+END PROGRAM euler_implicito
